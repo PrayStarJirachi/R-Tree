cab003f823d2f38d2da6ec2ac8ab4b538f2e7330
Jirachi <PrayStarJirachi@gmail.com>
2016-06-01 02:34:19 +0800

    Something changed.

--- R Tree/RTreeNode.hpp
+++ R Tree/RTreeNode.hpp
@@ -10,9 +10,9 @@ template<class M, class D>
 class RTreeNode{
 friend class RTree<M, D>;
 private:
-	void *child[M];
-	RTreeNode *father;
-	HyperRectangle box[D];
+	void *child[M + 1];
+	RTreeNode<M, D> *father;
+	HyperRectangle<D> box[M + 1];
 	size_t level;
 
 };
